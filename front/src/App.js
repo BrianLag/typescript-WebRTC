4ce65191cdf6a0ff0470e9dd7a49bb11d79940f4
Brian Lagaude <brian.lagaude@gmail.com>
2021-06-26 17:12:15 +0200

    add new packages import in app.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useEffect, useRef, useState } from "react"
+// import Button from "@material-ui/core/Button"
+// import IconButton from "@material-ui/core/IconButton"
+// import TextField from "@material-ui/core/TextField"
+// import AssignmentIcon from "@material-ui/icons/Assignment"
+// import PhoneIcon from "@material-ui/icons/Phone"
+// import { CopyToClipboard } from "react-copy-to-clipboard"
+// import Peer from "simple-peer"
+// import io from "socket.io-client"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Don't leave blank div or typescript will be angry</p>
     </div>
   );
 }
