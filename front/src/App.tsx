731ad5dea2b3b41a883ea14b57b46607645dcdd8
Brian Lagaude <brian.lagaude@gmail.com>
2021-06-26 19:24:08 +0200

    add video with useRef and useEffect

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "./App.css"
 
 function App() {
+  const [stream, setStream] = useState()
+  const myVideo = useRef<HTMLVideoElement>(null!);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({video: true, audio: true }).then((stream: any) => {
+      setStream(stream)
+      myVideo.current.srcObject = stream
+    })
+  }, [])
   return (
-    <div className="App">
-     <p>Don't leave blank div or typescript will be angry</p>
-    </div>
+    <>
+      <h1>Video Chat </h1>
+      <div className="container">
+        <div className="videoContainer">
+          <div className="video">
+           {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }}/>}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
